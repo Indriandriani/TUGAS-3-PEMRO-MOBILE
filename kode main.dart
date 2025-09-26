146ed8e50ebc25af89e34c6a1beef96e81b6142c
Indri_park <indriandriani673@gmail.com>
2025-09-26 22:02:22 +0700

    Create kode main.dart

---
+++ kode main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'column_example.dart';
+import 'row_example.dart';
+import 'form_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Layout Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter Layout Demo")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ColumnExample()),
+                );
+              },
+              child: const Text("Lihat Column Example"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RowExample()),
+                );
+              },
+              child: const Text("Lihat Row Example"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FormPage()),
+                );
+              },
+              child: const Text("Lihat Form Example"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
